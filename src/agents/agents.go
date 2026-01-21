e52aac3f00cbac82c864e7013fe3ece95255011f
altaev <altaevnurzhan1@gmail.com>
2026-01-21 16:08:46 +0500

    .

--- src/agents/agents.go
+++ src/agents/agents.go
@@ -2,6 +2,7 @@ package agents
 
 import (
 	"example/hello/src/position"
+	"slices"
 
 	"github.com/google/uuid"
 )
@@ -29,6 +30,35 @@ func CreateSimpleAgent(worldWidth int, worldHeight int) Agent {
 	}
 }
 
-func AgentWandering(agent *Agent, worldWidth int, worldHeight int) {
+func (agent *Agent) AgentWandering(worldWidth int, worldHeight int) {
 
+}
+
+func (agent *Agent) MoveAgent(grid map[position.SolidCell][]uuid.UUID, dt float64, worldWidth, worldHeight int) {
+	var cell = position.SolidCell{
+		X: int(agent.X),
+		Z: int(agent.Z),
+	}
+	agent.X = agent.X + agent.VX*dt
+	agent.Z = agent.Z + agent.VZ*dt
+
+	var newCell = position.SolidCell{
+		X: int(agent.X),
+		Z: int(agent.Z),
+	}
+
+	if newCell != cell {
+		index := slices.Index(grid[cell], agent.ID)
+		if index != -1 {
+			grid[cell] = slices.Delete(grid[cell], index, index+1)
+		}
+	}
+
+	if agent.X < 0 || agent.X > float64(worldWidth) {
+		agent.VX = -agent.VX
+	}
+
+	if agent.Z < 0 || agent.Z > float64(worldHeight) {
+		agent.VZ = -agent.VZ
+	}
 }
--- src/world/world.go
+++ src/world/world.go
@@ -3,7 +3,6 @@ package world
 import (
 	"example/hello/src/agents"
 	"example/hello/src/position"
-	"slices"
 
 	"github.com/google/uuid"
 )
@@ -19,34 +18,7 @@ type World struct {
 
 func (w *World) Tick(dt float64) {
 	for i := range w.Agents {
-		agent := &w.Agents[i]
-
-		var cell = position.SolidCell{
-			X: int(agent.X),
-			Z: int(agent.Z),
-		}
-		agent.X = agent.X + agent.VX*dt
-		agent.Z = agent.X + agent.VZ*dt
-
-		var newCell = position.SolidCell{
-			X: int(agent.X),
-			Z: int(agent.Z),
-		}
-
-		if newCell != cell {
-			index := slices.Index(w.Grid[cell], agent.ID)
-			if index != -1 {
-				w.Grid[cell] = slices.Delete(w.Grid[cell], index, index+1)
-			}
-		}
-
-		if agent.X < 0 || agent.X > float64(w.Width) {
-			agent.VX = -agent.VX
-		}
-
-		if agent.Z < 0 || agent.Z > float64(w.Width) {
-			agent.VZ = -agent.VZ
-		}
+		w.Agents[i].MoveAgent(w.Grid, dt, w.Width, w.Height)
 	}
 }
 
